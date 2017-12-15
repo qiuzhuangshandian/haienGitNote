166470ae5ee634711d8dd13326a19ff5501fe4b7
hai <h@email.com>
2017-12-15 15:45:18 +0800

    note on ubuntu 12_15

---
+++ gitNote.py
@@ -0,0 +1,27 @@
+# noting on ubuntu
+1 connect the local repository to remote
+	cmd:	git remote add origin <server>
+
+2 add all change to cache areas
+	cmd:	git add *
+
+3 push your branch to remote
+	cmd:	git push origin <branch>
+
+4 update your local repository to the up-to-date change
+	cmd:	git pull
+
+5 merge branch
+	cmd:	git merge <branch>
+
+6 built a tag 
+	cmd:	git tag 1.0.0 <ID>
+
+7 get log (ID)
+	cmd:	git log --oneline
+
+8 replace local file with file in repository
+	cmd:	git checkout -- <filename>
+
+9 open a graphical git
+	cmd gitk 
